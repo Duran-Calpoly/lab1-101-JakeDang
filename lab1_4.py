525c5cedc7573b6e42acbdc853e8a34d48c16613
JakeDang <jdang56@calpoly.edu>
2026-01-17 02:49:51 +0000

    Lab 1 Done

--- lab1_4.py
+++ lab1_4.py
@@ -1,1 +1,14 @@
-pass
+def calculate_average(num1, num2, num3):
+    average = (num1 + num2 + num3) / 3
+    return average
+
+def add_tax(bill_total):
+    total = bill_total + bill_total * 0.10 
+    return total
+
+def greet_user(name):
+    return "Hello " + name
+
+test_avg = calculate_average(1,2,3)
+print(test_avg)
+print(greet_user(input("What's your name?: ")))
